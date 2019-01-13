621a7e8002ce415aab8522e563eb761ec70c5ee8
Flameri <arto.lindgren1@gmail.com>
2019-01-13 09:12:31 +0200

    Started project

---
+++ WebApiExample/WebApiExample/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+         // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ WebApiExample/WebApiExample/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiExample.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            Phone = new HashSet<Phone>();
+        }
+
+        public Person(long id, string name, short? age, ICollection<Phone> phone)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Phone = phone;
+        }
+
+        public long Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public short? Age { get; set; }
+
+        [InverseProperty("Person")]
+        public virtual ICollection<Phone> Phone { get; set; }
+    }
+}
