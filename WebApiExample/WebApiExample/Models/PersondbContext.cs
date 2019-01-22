1f059dc0b9038abecea4cce517e9ca625dc76de1
Flameri <arto.lindgren1@gmail.com>
2019-01-22 11:14:21 +0200

    Added utility password hash

--- WebApiExample/WebApiExample/Models/PersondbContext.cs
+++ WebApiExample/WebApiExample/Models/PersondbContext.cs
@@ -22,8 +22,8 @@ namespace WebApiExample.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-K3DT06D7\\SQLEXPRESS;Initial Catalog=PersonDB;Integrated Security=True");
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+     //           optionsBuilder.UseSqlServer("Data Source=LAPTOP-K3DT06D7\\SQLEXPRESS;Initial Catalog=PersonDB;Integrated Security=True");
             }
         }
 
